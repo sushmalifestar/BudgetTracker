b96ebf7585422c88d84c5ab9215e88d3e8d305a3
Sai Sushma Cherukupalli <sushmalifestar@gmail.com>
2026-03-12 10:14:37 +0530

    Refactored TemplateForm to Reactive Form

--- src/app/components/transaction-form/transaction-form.component.ts
+++ src/app/components/transaction-form/transaction-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule, FormBuilder,FormArray,FormGroup, FormControl, Validators} from '@angular/forms';
 import { TransactionDraft } from 'src/app/models/transaction.model';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 import {
   IonInput,
   IonItem,
@@ -15,7 +16,7 @@ import {
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonInput,
     IonItem,
     IonLabel,
@@ -28,23 +29,39 @@ import {
 export class TransactionFormComponent implements OnChanges  {
   
   @Input() model!: TransactionDraft;
-  
-  localModel!:TransactionDraft;
-
   @Input() submitLabel = 'Save';
   @Input() showCancel = true;
-  
   @Output() save = new EventEmitter<typeof this.model>();
   @Output() cancel = new EventEmitter<void>();
 
   amountLimitExceeded = false;
+  largeTransaction =false;
+
+  constructor(private fb:FormBuilder){}
+
+  transactionForm= this.fb.group({
+    amount:[null as number | null ,[Validators.required, positiveAmountValidator]],
+    source: ['', Validators.required],
+    date:['', [Validators.required, noFutureDateValidator]]
+  })
+
+  get fDetails(){
+    return this.transactionForm.controls;
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['model'] && this.model) {
-      this.localModel = { ...this.model }; 
+      this.transactionForm.patchValue(this.model);
     }
   }
 
+  ngOnInit(){
+    this.transactionForm.get('amount')?.valueChanges.subscribe(amount=>{
+    if (amount === null) return;
+    this.largeTransaction = amount > 100000;
+    })
+  }
+  
   onAmountInput(event: any) {
     const value = event.target.value;
     if (!value) {
@@ -63,14 +80,47 @@ export class TransactionFormComponent implements OnChanges  {
   }
 
   onSubmit() {
-    if (!this.localModel.amount || this.localModel.amount <= 0) {
+    if (this.transactionForm.invalid) {
       return;
     }
-    this.save.emit(this.localModel);
+    const data = this.transactionForm.value as TransactionDraft;
+  this.save.emit(data);
   }
   
   onCancel() {
     this.cancel.emit();
   }
 
+}
+
+
+export function positiveAmountValidator(control: AbstractControl): ValidationErrors | null {
+
+  const value = control.value;
+
+  if (value === null) return null;
+
+  if (value <= 0) {
+    return { invalidAmount: true };
+  }
+
+  return null;
+}
+
+export function noFutureDateValidator(control: AbstractControl): ValidationErrors | null {
+
+  const value = control.value;
+
+  if (!value) return null;
+
+  const selectedDate = new Date(value);
+  const today = new Date();
+
+  today.setHours(0,0,0,0);
+
+  if (selectedDate > today) {
+    return { futureDate: true };
+  }
+
+  return null;
 }
